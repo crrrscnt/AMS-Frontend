3ee3f09d227880120d7a3a2d53f5a19cf3e07a69
crrrscnt <114849389+crrrscnt@users.noreply.github.com>
2024-11-07 02:38:02 +0800

    Delete src directory

--- src/Routes.tsx
+++
@@ -1,9 +0,0 @@
-export const ROUTES = {
-    HOME: "/",
-    SPACEOBJECTS: "/spaceobjects",
-}
-export type RouteKeyType = keyof typeof ROUTES;
-export const ROUTE_LABELS: {[key in RouteKeyType]: string} = {
-    HOME: "Главная",
-    SPACEOBJECTS: "Космические объекты",
-};
